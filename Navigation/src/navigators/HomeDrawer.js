2302579019eb1278cadd9991e81162b33e9f3d92
kaiorr <kaio.rocharibeiro@gmail.com>
2020-08-01 17:27:56 -0300

    feat:(drawerNavigator) controlando posicionamento, opacidade, área clicável para apresentação e tipo 🚧 📱

--- Navigation/src/navigators/HomeDrawer.js
+++ Navigation/src/navigators/HomeDrawer.js
@@ -2,13 +2,24 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import Home from '../screems/Home';
 import Perfil from '../screems/Perfil';
 
-const HomeDrawer = createDrawerNavigator({
-  Home: {
-    screen: Home,
+const HomeDrawer = createDrawerNavigator(
+  {
+    Home: {
+      screen: Home,
+    },
+    Perfil: {
+      screen: Perfil,
+    },
   },
-  Perfil: {
-    screen: Perfil,
+  {
+    drawerPosition: 'left',
+    drawerType: 'slide',
+    drawerWidth: 70,
+    drawerBackgroundColor: '#FF0000',
+    edgeWidth: 150,
+    hideStatusBar: true,
+    overlayColor: 0.5
   },
-});
+);
 
 export default HomeDrawer;
