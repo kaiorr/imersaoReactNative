d054d55f3d39feffd3a59997dee95d35ccc81542
kaiorr <kaio.rocharibeiro@gmail.com>
2020-08-16 19:00:22 -0300

    feat: initial commit listMovies req api

---
+++ listMovies/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Testando 1, 2 ,3 ...</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
